802b76a941672407f41d120573a4bb15a95172bc
R LAKSHMI NARASIMHA REDDY <lakshminarasimha0654@gmail.com>
2024-07-07 22:13:04 +0530

    Create Q21.py

---
+++ Q21.py
@@ -0,0 +1,9 @@
+class Solution:
+    """
+    Time:   O(log(n))
+    Memory: O(log(n))
+    """
+    def numberOfSteps(self, num: int) -> int:
+        if num == 0:
+            return 0
+        return 1 + self.numberOfSteps(num - 1 if num & 1 else num >> 1)
